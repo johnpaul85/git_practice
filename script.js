fa0a6996a62c6d3ff54cd321cc956fcc8b51b097
JP <typ_0@hotmail.com>
2021-02-05 17:35:00 -0500

    Fully Working Horoscope

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 
-today = mm+dd;
+const mmdd = parseInt(mm+dd,10);
 
 const zodiac = [
-    {name: 'Aires', dateStart: 0321, dateEnd: 0419, type: 'Fire'},
-    {name: 'Taurus', dateStart: 0420, dateEnd: 0520, type: 'Earth'},
-    {name: 'Gemini', dateStart: 0521, dateEnd: 0620, type: 'Air'},
-    {name: 'Cancer', dateStart: 0621, dateEnd: 0722, type: 'Water'},
-    {name: 'Leo', dateStart: 0723, dateEnd: 0822, type: 'Fire'},
-    {name: 'Virgo', dateStart: 0823, dateEnd: 0922, type: 'Earth'},
-    {name: 'Libra', dateStart: 0923, dateEnd: 1022, type: 'Air'},
+    {name: 'Aires', dateStart: 321, dateEnd: 419, type: 'Fire'},
+    {name: 'Taurus', dateStart: 420, dateEnd: 520, type: 'Earth'},
+    {name: 'Gemini', dateStart: 521, dateEnd: 620, type: 'Air'},
+    {name: 'Cancer', dateStart: 621, dateEnd: 722, type: 'Water'},
+    {name: 'Leo', dateStart: 723, dateEnd: 822, type: 'Fire'},
+    {name: 'Virgo', dateStart: 823, dateEnd: 922, type: 'Earth'},
+    {name: 'Libra', dateStart: 923, dateEnd: 1022, type: 'Air'},
     {name: 'Scorpio', dateStart: 1023, dateEnd: 1121, type: 'Water'},
     {name: 'Sagittarius', dateStart: 1122, dateEnd: 1221, type: 'Fire'},
-    {name: 'Capricorn', dateStart: 1222, dateEnd: 0119, type: 'Earth'},
-    {name: 'Aquarius', dateStart: 0120, dateEnd: 0218, type: 'Air'},
-    {name: 'Pisces', dateStart: 0219, dateEnd: 0320, type: 'Water'}
+    {name: 'Capricorn', dateStart: 1222, dateEnd: 119, type: 'Earth'},
+    {name: 'Aquarius', dateStart: 120, dateEnd: 218, type: 'Air'},
+    {name: 'Pisces', dateStart: 219, dateEnd: 320, type: 'Water'}
 ];
 
 const Fire = ['starting', 'enthusiastic', 'courageous'];
@@ -27,13 +27,16 @@ const Water = ['kind', 'open minded', 'wise'];
 
 const category = ['career', 'relationship', 'life'];
 
+let selectedType = '';
+let randomType = '';
+let randomCategory = '';
+let horoscope = 'Try again.';
+
 function generate(){
     const randomIndex1 = Math.floor(Math.random() * 3);
     const randomIndex2 = Math.floor(Math.random() * 3);
-    let selectedType = '';
-    let randomType = '';
     for(let i=0; i < zodiac.length+1; i++){
-        if(zodiac[i].dateStart <= today && zodiac[i].dateEnd >= today){
+        if(zodiac[i].dateStart < zodiac[i].dateEnd && zodiac[i].dateStart <= mmdd && zodiac[i].dateEnd >= mmdd || zodiac[i].dateStart > zodiac[i].dateEnd && zodiac[i].dateStart <= mmdd || zodiac[i].dateStart > zodiac[i].dateEnd && mmdd <= zodiac[i].dateEnd){
             if(zodiac[i].type === 'Fire'){
                 selectedType = 'Fire';
                 randomType = Fire[randomIndex1];
@@ -47,8 +50,8 @@ function generate(){
                 selectedType = 'Water';
                 randomType = Water[randomIndex1];
             };
-            const randomCategory = category[randomIndex2];
-            const horoscope = `${zodiac[i].name}: Your ${randomCategory} will be ${randomType}!`;
+            randomCategory = category[randomIndex2];
+            horoscope = `${zodiac[i].name}: Your ${randomCategory} will be ${randomType}!`;
             return horoscope;
         };
     };
